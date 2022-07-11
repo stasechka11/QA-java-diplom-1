32d338a8b8cfde11839c52df37e4e4f55370092d
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2022-07-11 22:17:24 +0300

    tests for Bun

---
+++ src/test/java/BunTest.java
@@ -0,0 +1,31 @@
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import praktikum.Bun;
+
+import java.util.Random;
+
+public class BunTest {
+    Bun bun;
+    String bunNameExpected;
+    Float bunPriceExpected;
+
+    @Before
+    public void setUp() {
+        bunNameExpected = RandomStringUtils.randomAlphabetic(6);
+        Random r = new Random();
+        bunPriceExpected = r.nextFloat();
+        bun = new Bun(bunNameExpected, bunPriceExpected);
+    }
+
+    @Test
+    public void bunGetNameReturnsText() {
+        Assert.assertEquals(bunNameExpected, bun.getName());
+    }
+
+    @Test
+    public void bunGetPriceReturnsFloat() {
+        Assert.assertEquals(bunPriceExpected, bun.getPrice(), 0);
+    }
+}
