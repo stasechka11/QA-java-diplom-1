6d0ddab1fa2a8722d3c28313e99b0da99196de0b
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2022-07-11 22:58:01 +0300

    tests for Ingredient

---
+++ src/test/java/IngredientTest.java
@@ -0,0 +1,40 @@
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
+import java.util.Random;
+
+public class IngredientTest {
+    Ingredient ingredient;
+    String ingredientNameExpected;
+    float ingredientPriceExpected;
+    IngredientType ingredientTypeExpected;
+
+    @Before
+    public void setUp() {
+        ingredientNameExpected = RandomStringUtils.randomAlphabetic(7);
+        Random r = new Random();
+        ingredientPriceExpected = r.nextFloat();
+        ingredientTypeExpected = praktikum.IngredientType.values()[r.nextInt(IngredientType.values().length)];
+
+        ingredient = new Ingredient(ingredientTypeExpected, ingredientNameExpected, ingredientPriceExpected);
+    }
+
+    @Test
+    public void ingredientGetNameReturnsText() {
+        Assert.assertEquals(ingredientNameExpected, ingredient.getName());
+    }
+
+    @Test
+    public void ingredientGetPriceReturnsFloat() {
+        Assert.assertEquals(ingredientPriceExpected, ingredient.getPrice(), 0);
+    }
+
+    @Test
+    public void ingredientGetTypeReturnsEnumValue() {
+        Assert.assertEquals(ingredientTypeExpected, ingredient.getType());
+    }
+}
