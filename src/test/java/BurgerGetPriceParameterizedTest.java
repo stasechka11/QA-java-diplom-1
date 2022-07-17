1a7f387a9e0732576c3882d52eb0da1f58934dae
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2022-07-17 20:48:15 +0300

    Parameterized test for method Burger.getPrice

---
+++ src/test/java/BurgerGetPriceParameterizedTest.java
@@ -0,0 +1,60 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import praktikum.Bun;
+import praktikum.Burger;
+import praktikum.Ingredient;
+
+
+@RunWith(Parameterized.class)
+public class BurgerGetPriceParameterizedTest {
+    Burger burger;
+    @Mock
+    Bun bun;
+    @Mock
+    Ingredient ingredient;
+
+    int ingredientCount;
+    float expectedPrice;
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    public BurgerGetPriceParameterizedTest(int ingredientCount, float expectedPrice) {
+        this.ingredientCount = ingredientCount;
+        this.expectedPrice = expectedPrice;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getBurgerIngredients() {
+        return new Object[][]{
+                {0, 200f},
+                {1, 400f},
+                {2, 600f}
+        };
+    }
+
+    @Before
+    public void setUp() {
+        burger = new Burger();
+        burger.setBuns(bun);
+    }
+
+    @Test
+    public void burgerGetPriceReturnsFloatTest() {
+        for (int i = 0; i < ingredientCount; i++) {
+            burger.addIngredient(ingredient);
+        }
+        Mockito.when(bun.getPrice()).thenReturn(100f);
+        Mockito.when(ingredient.getPrice()).thenReturn(200f);
+
+        Assert.assertEquals(expectedPrice, burger.getPrice(), 0.00f);
+    }
+}
